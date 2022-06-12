e3ba024c842917b93ede3bdfd279f7a021f22080
howshecodes <102827973+howshecodes@users.noreply.github.com>
2022-06-12 01:02:43 -0400

    conversin

---
+++ mainlec13.js
@@ -0,0 +1,15 @@
+ "use strict";
+//  type conversion
+// console.log("18" + String(1));
+// console.log("19" + 1);
+// console.log("19" + String(1));
+// console.log(typeof String(1));
+
+// const number = 100;
+// console.log(typeof number);
+// console.log(typeof String(number));
+
+let birthYear = prompt("Please any number");
+const currentYear = 2022;
+const age = 2022 - birthYear;
+console.log(age);
